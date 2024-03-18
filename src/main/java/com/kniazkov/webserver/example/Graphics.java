b04d09c3082c9664ad0442851c0ba44c3a64c109
Ivan Kniazkov <ivan@kniazkov.com>
2024-03-18 15:33:56 +0300

    Graphics example

---
+++ src/main/java/com/kniazkov/webserver/example/Graphics.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) 2024 Ivan Kniazkov
+ */
+package com.kniazkov.webserver.example;
+
+import com.kniazkov.webserver.Handler;
+import com.kniazkov.webserver.Options;
+import com.kniazkov.webserver.Response;
+import com.kniazkov.webserver.Server;
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+
+/**
+ * Web server that generates some graphics and outputs them in PNG format.
+ * How to use:
+ *   1. Run the program;
+ *   2. Open your browser and type "<a href="http://localhost:8000">...</a>" in the address bar.
+ */
+public class Graphics {
+    /**
+     * Starting point.
+     * @param args Program arguments
+     */
+    public static void main(String[] args) {
+        final Options options = new Options();
+        final BufferedImage image = new BufferedImage(
+                500,
+                500,
+                BufferedImage.TYPE_3BYTE_BGR
+        );
+        final Graphics2D graphics = (Graphics2D) image.getGraphics();
+        graphics.setColor(Color.RED);
+        graphics.drawOval(50 , 50, 400, 400);
+        graphics.drawOval(120 , 100, 100, 100);
+        graphics.drawOval(280 , 100, 100, 100);
+        graphics.drawArc(100, 100, 300, 300, 180, 180);
+        final ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        try {
+            ImageIO.write(image, "png", stream);
+        } catch (IOException ignored) {
+        }
+        final byte[] bytes = stream.toByteArray();
+        final Handler handler = request -> new Response() {
+            @Override
+            public String getContentType() {
+                return "image/png";
+            }
+
+            @Override
+            public byte[] getData() {
+                return bytes;
+            }
+        };
+
+        Server.start(options, handler);
+    }
+}
