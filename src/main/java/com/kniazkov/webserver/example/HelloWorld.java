8d4acdab31f2c38e712eaf3c8642d342373a368a
Ivan Kniazkov <ivan@kniazkov.com>
2024-03-18 12:15:42 +0300

    Hello world example

---
+++ src/main/java/com/kniazkov/webserver/example/HelloWorld.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2024 Ivan Kniazkov
+ */
+package com.kniazkov.webserver.example;
+
+import com.kniazkov.webserver.Handler;
+import com.kniazkov.webserver.Options;
+import com.kniazkov.webserver.Request;
+import com.kniazkov.webserver.Response;
+import com.kniazkov.webserver.ResponseText;
+import com.kniazkov.webserver.Server;
+
+/**
+ * Web server that returns text document with "hello world" content.
+ * How to use:
+ *   1. Run the program;
+ *   2. Open your browser and type "<a href="http://localhost:8000">...</a>" in the address bar.
+ */
+public class HelloWorld {
+    /**
+     * Starting point.
+     * @param args Program arguments
+     */
+    public static void main(String[] args) {
+        Options options = new Options();
+        Handler handler = new Handler() {
+
+            @Override
+            public Response handle(Request request) {
+                return new ResponseText("hello world");
+            }
+        };
+
+        Server.start(options, handler);
+    }
+}
---
+++ src/main/java/com/kniazkov/webserver/example/package-info.java
@@ -0,0 +1,9 @@
+/*
+ * Copyright (c) 2024 Ivan Kniazkov
+ */
+
+/**
+ * This package contains examples of web server implementations
+ * using the library {@link com.kniazkov.webserver}.
+ */
+package com.kniazkov.webserver.example;
