e78cf5d361f3d39f8a339c02b70c0342c99272e0
Ivan Kniazkov <ivan@kniazkov.com>
2026-08-08 00:35:32 +0300

    Request

---
+++ src/main/java/com/kniazkov/webserver/Request.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright (c) 2026 Ivan Kniazkov
+ */
+
+package com.kniazkov.webserver;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Represents an HTTP request.
+ * <p>
+ * Implementations of this interface are immutable. All collections returned
+ * by this interface are immutable.
+ */
+public interface Request {
+
+    /**
+     * Returns the request headers.
+     *
+     * @return
+     *     the request headers.
+     */
+    RequestHeaders getHeaders();
+
+    /**
+     * Returns parameters obtained from the request target query string.
+     *
+     * @return
+     *     an immutable map containing immutable lists of query parameter
+     *     values.
+     */
+    Map<String, List<String>> getQuery();
+
+    /**
+     * Returns form parameters obtained from the request body.
+     * <p>
+     * For GET requests, this map is empty.
+     *
+     * @return
+     *     an immutable map containing immutable lists of form parameter
+     *     values.
+     */
+    Map<String, List<String>> getForm();
+
+    /**
+     * Returns files uploaded as part of the request.
+     *
+     * @return
+     *     an immutable map containing immutable lists of uploaded files.
+     */
+    Map<String, List<UploadedFile>> getFiles();
+
+    /**
+     * Returns cookies supplied with the request.
+     *
+     * @return
+     *     an immutable map of cookie names to values.
+     */
+    Map<String, String> getCookies();
+
+    /**
+     * Returns the original request body.
+     * <p>
+     * A new copy of the underlying byte array is created on every invocation.
+     * Modifying the returned array does not affect this request.
+     *
+     * @return
+     *     a copy of the original request body.
+     */
+    byte[] getBody();
+}
